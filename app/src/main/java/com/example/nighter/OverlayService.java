6dd6df67c86201800f733afa95ecb66a5c10bd5c
TaeHwan KIM <tomxoghks789@naver.com>
2021-08-12 17:08:34 +0900

    Make OverlayService cover whole device.

--- app/src/main/java/com/example/nighter/OverlayService.java
+++ app/src/main/java/com/example/nighter/OverlayService.java
@@ -6,7 +6,6 @@ import android.graphics.Color;
 import android.graphics.PixelFormat;
 import android.os.IBinder;
 import android.util.DisplayMetrics;
-import android.util.Log;
 import android.view.WindowManager;
 import android.widget.LinearLayout;
 
@@ -40,12 +39,10 @@ public class OverlayService extends Service {
         if (overlayView != null) {
             WindowManager wm = (WindowManager) getSystemService(WINDOW_SERVICE);
             wm.removeView(overlayView);
-            Log.i("KIM", "Disabled!");
         }
     }
 
     public void RunOverlayService() {
-        Log.i("KIM", "Running!");
         overlayView = new LinearLayout(this);
         overlayView.setBackgroundColor(Color.parseColor("#000000"));
         DisplayMetrics metrics = getResources().getDisplayMetrics();
@@ -55,30 +52,12 @@ public class OverlayService extends Service {
         int y = 0;
         WindowManager.LayoutParams params = new WindowManager.LayoutParams((width > height) ? width : height, (width > height) ? width : height, x, y,
                 WindowManager.LayoutParams.TYPE_APPLICATION_OVERLAY,
-                WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE | WindowManager.LayoutParams.FLAG_NOT_TOUCHABLE,
+                WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE
+                        | WindowManager.LayoutParams.FLAG_NOT_TOUCHABLE
+                        | WindowManager.LayoutParams.FIRST_SYSTEM_WINDOW,
                 PixelFormat.TRANSLUCENT);
         WindowManager wm = (WindowManager) getSystemService(WINDOW_SERVICE);
         overlayView.setAlpha(brightness);
-        overlayView.setPadding(0, 0, 0, 0);
         wm.addView(overlayView, params);
     }
-
-//    public int getStatusBarHeight() {
-//        int result = 0;
-//        int resourceId = getApplicationContext().getResources().getIdentifier("status_bar_height", "dimen", "android");
-//        if (resourceId > 0) {
-//            result = getApplicationContext().getResources().getDimensionPixelSize(resourceId);
-//        }
-//        return result;
-//    }
-//
-//    public int getNaviBarHeight(){
-//        int result = 0;
-//        Resources resources = getApplicationContext().getResources();
-//        int resourceId = resources.getIdentifier("navigation_bar_height", "dimen", "android");
-//        if (resourceId > 0) {
-//            result = resources.getDimensionPixelSize(resourceId);
-//        }
-//        return result;
-//    }
 }
